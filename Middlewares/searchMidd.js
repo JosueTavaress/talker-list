0b74adba54a7992fd5b7fcc548433c89eec1b1d1
JosueTavaress <josueferreira8.jf@gmail.com>
2022-02-19 00:49:07 -0300

    remove tests

--- Middlewares/searchMidd.js
+++ Middlewares/searchMidd.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
+const characters = require('../models/characters');
 
 module.exports = async (request, response) => {
   const { id } = request.params;
-  const talker = await fs.promises.readFile('./talker.json');
-  const parseJson = JSON.parse(talker);
-  const userFilter = parseJson.find((el) => el.id === Number(id));
+
+  const talkerList = await characters.getAll();
+
+  const userFilter = talkerList.find((el) => el.id === Number(id));
   return userFilter ? response.status(200).send(userFilter)
   : response.status(404).send({ message: 'Pessoa palestrante não encontrada' });
 };
--- Middlewares/talkerMidd.js
+++ Middlewares/talkerMidd.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
+const characters = require('../models/characters');
 
-module.exports = async (_request, response) => {
-   const talker = await fs.promises.readFile('./talker.json', 'utf8');
-   const talkers = JSON.parse(talker);
-   const responseUser = talker.length > 0 ? response.status(200).send(talkers)
+module.exports = async (_request, response) => {   
+   const talkerList = await characters.getAll();
+
+   const responseUser = talkerList.length > 0 ? response.status(200).send(talkerList)
      : response.status(200).send([]);
      return responseUser;
 };
